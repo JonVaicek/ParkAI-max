f8dcbdc1d87b88d20aa69f781b257cf6610875b8
JV <jonasvaice@gmail.com>
2026-02-09 20:59:16 +0000

    debugging

--- libdeepvision/include/streammuxer.h
+++ libdeepvision/include/streammuxer.h
@@ -73,7 +73,7 @@ class StreamMuxer{
     uint64_t fd[10];
     bool run=true;
 
-    static const int MAX_EVENTS = 256;
+    static const int MAX_EVENTS = 1024;
     epoll_event events[MAX_EVENTS];
     int epfd = -1;
 
