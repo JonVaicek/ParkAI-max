f34408615dd7fac196531d54d58cbc56a24d159e
JV <jonasvaice@gmail.com>
2026-02-10 11:51:27 +0000

    debugging

--- libdeepvision/src/streammuxer.cpp
+++ libdeepvision/src/streammuxer.cpp
@@ -178,14 +178,14 @@ int StreamMuxer::child_epoller(void){
 
         for (uint32_t i = 0; i < sources.size(); i++){
             auto *s = sources[i];
-            if(s->deinit_ && s->is_registered()){
-                    std::cout << "Removing evfd " << s->get_evfd() << "from epoll\n";
-                    epoll_ctl(epfd, EPOLL_CTL_DEL, s->get_evfd(), nullptr);
-                    s->set_epoll_flag(false);
-            }
-            if(s->deinit_ && !s->is_registered() && !s->killed_){
-                s->soft_deinit();
-            }
+            // if(s->deinit_ && s->is_registered()){
+            //         std::cout << "Removing evfd " << s->get_evfd() << "from epoll\n";
+            //         epoll_ctl(epfd, EPOLL_CTL_DEL, s->get_evfd(), nullptr);
+            //         s->set_epoll_flag(false);
+            // }
+            // if(s->deinit_ && !s->is_registered() && !s->killed_){
+            //     s->soft_deinit();
+            // }
 
             // if(s->is_closed() && s->killed_){
             //     if(s->is_past_timeout()){
