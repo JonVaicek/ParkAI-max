bd41474957342442c98a1bab46ba06cd41b4c07e
JV <jonasvaice@gmail.com>
2026-02-10 18:46:22 +0000

    debugging

--- libdeepvision/src/streammuxer.cpp
+++ libdeepvision/src/streammuxer.cpp
@@ -139,6 +139,7 @@ int StreamMuxer::child_epoller(void){
     std::vector <GstChildWorker *> to_kill;
     std::vector <GstChildWorker *> survivors;
     while (true){
+        
     if(!sources.empty()){
     if(mlock.try_lock()){
         bool epoll_del = false;
@@ -212,7 +213,11 @@ int StreamMuxer::child_epoller(void){
         to_kill.clear();
         to_kill = survivors;
         survivors.clear();
-    }}
+    }
+}
+else{
+    std::cout << "empty sources\n";
+}
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
         // for (uint32_t i = 0; i < sources.size(); i++){
         //     auto *s = sources[i];
