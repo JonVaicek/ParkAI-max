2a679b4c158cf015babd197d9c06ded8319fd905
JV <jonasvaice@gmail.com>
2026-02-12 22:05:08 +0000

    debugging

--- libdeepvision/camstream/src/camstream.cpp
+++ libdeepvision/camstream/src/camstream.cpp
@@ -575,11 +575,11 @@ static GstPadProbeReturn frame_probe_cb(GstPad *pad, GstPadProbeInfo *info, gpoi
 
     
     if ( !ctl->image || ctl->im_size < map.size ) {
-        void* p = (unsigned char*)realloc(ctl->image, map.size);
-        if (!p){
-            gst_buffer_unmap(buffer, &map);
-            return GST_PAD_PROBE_OK; // or handle OOM differently
+        if(ctl->image){
+            printf("[child - %s] reallocating memory \n");
+            free(ctl->image);
         }
+        ctl->image = (unsigned char *)malloc(map.size);
         ctl->im_size = map.size;
         ctl->imgW = width;
         ctl->imgH = height;
